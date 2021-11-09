95c0398fb8fa513c281c81fe75f6dfdeda901c51
Perso <jilan.menzer@yahoo.fr>
2021-11-09 19:28:32 +0100

    add route to post tweets about pepe the frogs markets

---
+++ pages/api/tweetMcap.js
@@ -0,0 +1,40 @@
+import { checkAuthAndRequest } from "lib/checkAuthAndRequest";
+import { getManyInDB } from "lib/handleDB";
+import { nowInTimestamp } from "lib/time";
+import { postTweet, getDataTweet } from "lib/tweet";
+
+const handlerTweetMcap = async (req, res) => {
+	try {
+		checkAuthAndRequest(req);
+		const now = nowInTimestamp();
+		const tenMinutes = 600;
+		const mostRecentRarepepeData = await getManyInDB(
+			"globalMarketsData",
+			{ collection: "rarepepe", timestamp: { $gt: now - tenMinutes } },
+			{}
+		);
+		const mostRecentFakerareData = await getManyInDB(
+			"globalMarketsData",
+			{ collection: "fakerare", timestamp: { $gt: now - tenMinutes } },
+			{}
+		);
+		const rarepepeDatasTweet = getDataTweet(mostRecentRarepepeData, "rarepepe");
+		const fakerareDatasTweet = getDataTweet(mostRecentFakerareData, "fakerare");
+		const rarepepePost = await postTweet(rarepepeDatasTweet);
+		const fakerarePost = await postTweet(fakerareDatasTweet);
+
+		return res.status(200).json({
+			action: "Tweet  MarketCap !",
+			rarepepePost,
+			fakerarePost,
+			succes: true,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({
+			error,
+		});
+	}
+};
+
+export default handlerTweetMcap;
