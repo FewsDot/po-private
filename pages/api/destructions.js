1231357c2c0ccf07e5b802cac2c48344572b9521
Perso <jilan.menzer@yahoo.fr>
2021-11-09 11:01:37 +0100

    fix destruction controller

--- pages/api/destructions.js
+++ pages/api/destructions.js
@@ -1,9 +1,39 @@
 import { checkAuthAndRequest } from "lib/checkAuthAndRequest";
+import listOfFakerare from "lib/listOfFakerare.js";
+import listOfRarepepe from "lib/listOfRarepepe.js";
+import { getAssetFromXchain } from "lib/fetcher.js";
+import { getArrayOfResultFromXChain } from "lib/formatData.js";
+import { getBulkOfUpdate } from "lib/getBulkOfUpdate.js";
+import { updateBulkInDB } from "lib/handleDB.js";
 
-const handler = async (req, res) => {
+const handlerDestructions = async (req, res) => {
 	try {
 		checkAuthAndRequest(req);
-	} catch (error) {}
+		const collection = req.query.collection;
+		if (!collection) throw "please provide a collection name";
+		const collectionToFetch = collection === "rarepepe" ? listOfRarepepe : listOfFakerare;
+		const arrayOfCardsDestructions = await getArrayOfResultFromXChain(
+			collectionToFetch,
+			getAssetFromXchain,
+			"destructions"
+		);
+		const destructedCardFiltred = arrayOfCardsDestructions.filter((card) => card.quantity > 0);
+		const bulkOfUpdate = getBulkOfUpdate(destructedCardFiltred, "destructions");
+		await updateBulkInDB(collection, bulkOfUpdate);
+
+		return res.status(200).json({
+			action: "Update Destructions",
+			NumberOfCard: collectionToFetch.length,
+			NumberOfCardToUpdate: destructedCardFiltred.length,
+			destructedCardFiltred,
+			succes: true,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({
+			error,
+		});
+	}
 };
 
-export default handler;
+export default handlerDestructions;
